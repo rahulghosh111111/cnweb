54fe50300d92739898d39e0c684a16cfa85f07b7
Sanskar Singh <163184569+MeSanskarSingh@users.noreply.github.com>
2025-06-10 03:11:03 +0530

    Create [eventName].js

---
+++ src/app/events/[eventName].js
@@ -0,0 +1,25 @@
+import { useRouter } from "next/router";
+import EventComponent from "../../components/EventComponent";
+import { events } from "../data/eventsData";
+
+const EventPage = () => {
+  const router = useRouter();
+  const { eventName } = router.query;
+
+  const eventIndex = events.findIndex((e) => e.slug === eventName);
+  const event = events[eventIndex];
+
+  if (!event) return <p className="text-center mt-10 text-red-600">Event not found</p>;
+
+  return (
+    <div>
+      <EventComponent
+        {...event}
+        positionIndex={eventIndex}
+        totalEvents={events.length}
+      />
+    </div>
+  );
+};
+
+export default EventPage;
