4d3057ac8670b7021ce0b8ab5a28ed461e94ea69
Soumyadip Maity <22053029@kiit.ac.in>
2025-06-09 17:56:32 +0530

    add Navbar component to RootLayout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Menubars/Footer";
+import Navbar from "@/components/Menubars/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -39,6 +40,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Navbar/>
         {children}
         <Footer/>
       </body>
