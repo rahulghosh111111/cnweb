6f11d200ac90e469d3e603895ce61704534b959f
Soumyadip Maity <22053029@kiit.ac.in>
2025-06-13 17:51:04 +0530

    Update import paths for HeroSection and MemberScroll components

--- src/app/members/page.js
+++ src/app/members/page.js
@@ -1,22 +1,27 @@
 'use client';
 
 import DomainScroll from '@/components/Members/DomainScroll';
-import HeroSection from '@/components/Members/HeroSection';
+import HeroSection from '@/components/membersUpdated/HeroSection';
+import MemberScroll from '@/components/membersUpdated/MemberScroll';
 import React,{useState, useRef} from 'react';
 
 export default function Members() {
   const [currentDomain , setCurrentDomain] = useState(0);
   const scrollContainerRef = useRef(null);
   return (
-    <div className="bg-[linear-gradient(180deg,#9B5E4E_0%,#550909_100%)]">
-      <HeroSection />
-      <div className="pt-6 pb-12 md:pt-12 md:pb-20">
+    <div className="">
+      {/* <HeroSection /> */}
+      {/* <div className="pt-6 pb-12 md:pt-12 md:pb-20">
         <DomainScroll 
       currentDomain={currentDomain}
       setCurrentDomain={setCurrentDomain}
       scrollContainerRef={scrollContainerRef}
       />
-      </div>
+      </div> */}
+
+      <HeroSection/>
+
+      <MemberScroll/>
     </div>
   );
 }
