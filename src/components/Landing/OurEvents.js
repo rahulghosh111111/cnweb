aa8576e7c4e6fd903f4282ee76dde9f7ab9cae78
Soumyadip Maity <22053029@kiit.ac.in>
2025-06-13 20:46:45 +0530

    Enhance EventsSection layout with updated styles, improved heading structure, and gradient button

--- src/components/Landing/OurEvents.js
+++ src/components/Landing/OurEvents.js
@@ -9,7 +9,7 @@ import { ArrowRight } from 'lucide-react';
 export default function EventsSection() {
   return (
     <section
-      className="w-full min-h-[600px] relative flex flex-col items-center overflow-hidden"
+      className="w-full min-h-[700px] relative flex flex-col justify-center items-center overflow-hidden"
       style={{
         backgroundPosition: 'center',
         backgroundSize: 'cover',
@@ -28,12 +28,14 @@ export default function EventsSection() {
       {/* Enhanced gradient overlay with CSS animation */}
       <div className="absolute inset-0 bg-gradient-to-br from-cyan-600/80 via-emerald-500/70 to-teal-700/80 z-0 " />
 
-      <h2
-        className="text-4xl md:text-6xl lg:text-7xl mb-8 mt-12 md:mt-20 z-10 text-center font-['Gang_of_Three',Arial,sans-serif] text-[#ffe08a] tracking-wider animate-fadeIn"
+        <h1
+        className="text-4xl sm:text-6xl  mb-20 mt-12 md:mt-16 z-10 text-center font-['Gang_of_Three',Arial,sans-serif] text-[#ffe08a] tracking-wider animate-fadeIn"
 
       >
-        OUR EVENTS
-      </h2>
+        OUR   <span className="font-black text-4xl sm:text-6xl leading-tight tracking-tight bg-gradient-to-r from-[#da3315] to-[#a90303] bg-clip-text text-transparent">
+          EVENTS
+        </span>
+      </h1>
 
       <div
         className="relative mb-10 md:mb-20 z-10 flex flex-col md:flex-row items-center justify-center w-full max-w-5xl px-4 md:px-8 gap-8 lg:gap-12 animate-scaleIn"
@@ -64,7 +66,7 @@ export default function EventsSection() {
             <div className="transform transition-all duration-300 hover:scale-105 cursor-pointer hover:shadow-glow">
               <Link 
                 href="/events"
-                className="bg-[#ffe08a] text-[#37742a] font-bold py-1.5 px-6 rounded-lg border-2 border-[#ffe08a]/30 shadow-lg flex items-center no-underline"
+                className="bg-gradient-to-r from-[#da3315] to-[#a90303]  font-bold py-1.5 px-6 rounded-lg border-2 border-[#da3315]/30 shadow-lg flex items-center no-underline"
               >
                 Explore Events <ArrowRight className="inline ml-2 h-5 w-5" strokeWidth={2.5} />
               </Link>
