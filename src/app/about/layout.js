ec2fb87a7f2a2d87457aded02d709c0f99b1a441
Soumyadip Maity <22053029@kiit.ac.in>
2025-06-19 02:00:04 +0530

    Add layout.js for About Us page with metadata and font integration

---
+++ src/app/about/layout.js
@@ -0,0 +1,49 @@
+
+import { Geist, Geist_Mono } from "next/font/google";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+
+  title: "About Us | CNKIIT",
+  description: "Coding Ninjas: KIIT Chapter is a vibrant community of tech enthusiasts dedicated to enhancing programming skills, fostering collaboration, and contributing to open source projects. Join us to learn, innovate, and grow together.",
+  keywords: "Coding Ninjas, KIIT, Chapter, Programming, Community , Events, Learning ,  Open Source",
+  authors: [{ name: "Coding Ninjas KIIT Chapter" }], 
+  description: "Join the Coding Ninjas KIIT Chapter to enhance your programming skills, participate in events, and contribute to open source projects. Connect with like-minded individuals and grow your knowledge in a collaborative environment.",
+  openGraph: {
+    title: "About Us | CNKIIT",
+    description: "Join the Coding Ninjas KIIT Chapter to enhance your programming skills, participate in events, and contribute to open source projects. Connect with like-minded individuals and grow your knowledge in a collaborative environment.",
+    url: "https://cnkiit.in",
+    siteName: "About Us | CNKIIT",
+    images: [
+      {
+        url: "https://cnkiit.in/logo.png",
+        width: 800,
+        height: 600,
+        alt: "Coding Ninjas: KIIT Chapter Logo",
+      },
+    ],
+  },
+
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+    
+        {children}
+      </body>
+    </html>
+  );
+}
