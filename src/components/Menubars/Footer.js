d319f9f754b50adb86aba54c934525229023dc60
Soumyadip Maity <22053029@kiit.ac.in>
2025-06-20 14:04:53 +0530

    Integrate AnimatedTooltipPreview component into Footer for enhanced contributor visibility

--- src/components/Menubars/Footer.js
+++ src/components/Menubars/Footer.js
@@ -8,19 +8,19 @@ import {
   MapPin,
 } from "lucide-react";
 import Image from "next/image";
+import { AnimatedTooltipPreview } from "../Landing/Contribution";
 
 const Footer = () => {
   // Store content in arrays for easier management
   const quickLinks = [
     { title: "About", url: "/about" },
     { title: "Home", url: "/" },
-    { title: "Privacy", url: "/privacy" },
-    { title: "Terms", url: "/terms" },
+    { title: "Members", url: "/members" },
+    { title: "Events", url: "/events" },
   ];
 
   const contactInfo = [
     { title: "Email", value: "", icon: <Mail size={16} /> },
-    { title: "Ph. No.", value: "", icon: <Phone size={16} /> },
     { title: "KIIT UNIVERSITY", value: "", icon: <MapPin size={16} /> },
     { title: "BHUBANESHWAR - 751024", value: "" },
     { title: "ODISHA, INDIA", value: "" },
@@ -33,9 +33,9 @@ const Footer = () => {
   ];
 
   return (
-    <footer className="relative w-full bg-gradient-to-b from-[#DC8068] via-[#C2463C] to-[#8D0E0E] text-white pt-6 px-[5%] overflow-hidden min-h-[600px] shadow-lg">
+    <footer className="relative w-full bg-gradient-to-b from-[#DC8068] via-[#C2463C] to-[#8D0E0E] text-white pt-6 sm:px-[5%] px-0 overflow-hidden min-h-[600px] shadow-lg">
       <div className="flex flex-wrap justify-between md:justify-center lg:gap-16 relative z-10 pb-[120px] max-w-7xl mx-auto">
-        <div className="flex-1 min-w-[280px] mr-8 lg:mb-8 p-6 rounded-lg">
+        <div className="flex-1 min-w-[280px] sm:mr-8 lg:mb-8 p-6 rounded-lg">
           <h2 className="text-[clamp(1.8rem,4vw,2.5rem)] mb-6 font-bold text-white">
             Unleash Your Questions
             <br />
@@ -90,14 +90,17 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      <div className="absolute bottom-0 left-0 w-full z-[1]">
+      <div className="absolute bottom-0 left-0 w-full z-10">
         <Image
           width={2000}
           height={1000}
           src="/FooterImg.png"
           alt="Ninja characters"
-          className="w-full h-auto block"
+          className="sm:w-full w-auto h-[130px] sm:h-auto block"
         />
+      </div>
+        <div className="absolute -mt-12 lg:mt-14 xl:mt-28 w-full left-0 z-[500] flex justify-center items-end ">
+        <AnimatedTooltipPreview />
       </div>
     </footer>
   );
