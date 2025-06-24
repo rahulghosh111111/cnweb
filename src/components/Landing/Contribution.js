b42357332a84938f246467ee0315cb3be5e812fa
Soumyadip Maity <22053029@kiit.ac.in>
2025-06-25 01:09:40 +0530

    Update contribution profiles in Contribution.js with new member details and images

--- src/components/Landing/Contribution.js
+++ src/components/Landing/Contribution.js
@@ -4,46 +4,48 @@ import { AnimatedTooltip } from "@/ui/tool-tip";
 const people = [
   {
     id: 1,
-    name: "John Doe",
-    designation: "Software Engineer",
-    image:
-      "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
+    name: "Soumyadip Maity",
+    designation: "Lead Developer",
+    image: "/WEB_Soumyadip.jpg",
   },
   {
     id: 2,
-    name: "Robert Johnson",
-    designation: "Product Manager",
-    image:
-      "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+    name: "Ayush ",
+    designation: "Web Developer",
+    image: "/WEB_Ayush.jpg",
   },
   {
     id: 3,
-    name: "Jane Smith",
-    designation: "Data Scientist",
-    image:
-      "https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YXZhdGFyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+    name: "Rupali Pasa",
+    designation: "UX Designer",
+    image: "/DESIGN_Rupali_Pasa.jpg",
   },
+
   {
     id: 4,
-    name: "Emily Davis",
-    designation: "UX Designer",
-    image:
-      "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGF2YXRhcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    name: "Sanskar Singh",
+    designation: "Web Developer",
+    image: "/WEB_Sanskar_Singh.jpeg",
   },
   {
     id: 5,
-    name: "Tyler Durden",
-    designation: "Soap Developer",
-    image:
-      "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80",
+    name: "Eshita Kapat",
+    designation: "Web Developer",
+    image: "/WEB_Eshita_Kapat.jpeg",
   },
   {
     id: 6,
-    name: "Dora",
-    designation: "The Explorer",
-    image:
-      "https://images.unsplash.com/photo-1544725176-7c40e5a71c5e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3534&q=80",
+    name: "Anusuya Srivastava",
+    designation: "UI/UX Designer",
+    image: "/DESIGN_Anusuya_Srivastava.jpg",
   },
+  {
+    id: 7,
+    name: "Rahul Ghosh",
+    designation: "Web Developer",
+    image: "/WEB_Rahul_Ghosh.jpg",
+  },
+
 ];
 
 export function AnimatedTooltipPreview() {
