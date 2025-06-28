decefacaf047f43bfe9d1c4a8accfd943d049ebc
Soumyadip Maity <22053029@kiit.ac.in>
2025-06-28 16:50:31 +0530

    Add leadership and domains data to memberData2025.js

---
+++ src/components/data/memberData2025.js
@@ -0,0 +1,565 @@
+import { Bot, Code, Smartphone, Shield } from "lucide-react";
+
+export const leadership = [
+  { 
+    id: 2, 
+    name: "Ayush Srivastava", 
+    position: "President", 
+    image: "/ayush_shrivastava.png",
+    social: {
+      instagram: "#",
+      linkedin: "#",
+      github: "#",
+    }
+  },
+  { 
+    id: 3, 
+    name: "Rahul Lenka", 
+    position: "Vice President", 
+    image: "/rahul_lenka.png",
+    social: {
+      instagram: "#",
+      linkedin: "#",
+      github: "#",
+    }
+  },
+];
+
+export const domains = {
+  "Web Development": {
+    icon: Code,
+    color: "from-[#8d0e0e] to-[#fa5935]",
+    lead: {
+      name: "Ojanshu Pati Tripathi",
+      role: "LEAD",
+      image: "/WEB_Ojanshu_Pati_Tripathi.jpeg",
+      social: {
+        instagram: "https://www.instagram.com/0janshu/",
+        linkedin: "https://www.linkedin.com/in/ojanshu-pati-tripathi/",
+        github: "https://github.com/ojanshu",
+      },
+    },
+    members: [
+      {
+        name: "Soumyadip Maity",
+        image: "/WEB_Soumyadip.jpg",
+        social: {
+          instagram: "https://www.instagram.com/smilingsoumya1/",
+          linkedin: "https://www.linkedin.com/in/soumyadip-maity-a77b41282/",
+          github: "https://github.com/Soumyadipmaity1",
+        },
+      },
+      {
+        name: "Aayush",
+        image: "/WEB_Ayush.jpg",
+        social: {
+          instagram: "https://www.instagram.com/ig._aayush/",
+          linkedin: "https://www.linkedin.com/in/aayushj19",
+          github: "https://github.com/aayushj19/",
+        },
+      },
+      {
+        name: "Sanskar Singh",
+        image: "/WEB_Sanskar_Singh.jpeg",
+        social: {
+          instagram: "https://www.instagram.com/mesanskarsingh/",
+          linkedin: "https://www.linkedin.com/in/mesanskarsingh/",
+          github: "https://github.com/MeSanskarSingh",
+        },
+      },
+      {
+        name: "Eshita Kapat",
+        image: "/WEB_Eshita_Kapat.jpeg",
+        social: {
+          instagram: "#",
+          linkedin: "https://www.linkedin.com/in/eshita-kapat",
+          github: "https://github.com/eshitakapat",
+        },
+      },
+      {
+        name: "Rahul Ghosh",
+        image: "/WEB_Rahul_Ghosh.jpg",
+        social: {
+          instagram: "https://www.instagram.com/gili_gili_6u",
+          linkedin: "https://www.linkedin.com/in/rahul-ghosh-b0b660254",
+          github: "#",
+        },
+      },
+      {
+        name: "Kaushik Verma",
+        image: "/WEB_Kaushik_Verma.jpg",
+        social: {
+          instagram: "#",
+          linkedin: "https://www.linkedin.com/in/kaushik-verma-2b5515254/",
+          github: "https://github.com/kaushik-2318/",
+        },
+      },
+      {
+        name: "Archi Shaw",
+        image: "/WEB_Archi_Shaw.jpg",
+        social: {
+          instagram: "#",
+          linkedin: "https://www.linkedin.com/in/archi-shaw-279a34277",
+          github: "#",
+        },
+      },
+    ],
+  },
+  "AI / Machine Learning": {
+    icon: Bot,
+    color: "from-[#fa5935] to-[#FF8820]",
+    lead: {
+      name: "Keshav dudani",
+      role: "LEAD",
+      image: "/demo.png",
+      social: {
+        instagram: "#",
+        linkedin: "#",
+        github: "#",
+      },
+    },
+    members: [
+      {
+        name: "Akshat Gupta",
+        image: "/ML_Akshat_Gupta.jpg",
+        social: {
+          instagram: "#",
+          linkedin: "https://www.linkedin.com/in/akshatgupta105/",
+          github: "https://github.com/Akshat19Ak",
+        },
+      },
+      {
+        name: "Arpita Aparna Sahay",
+        image: "/ML_Arpita_Aparna_Sahay.jpg",
+        social: {
+          instagram: "#",
+          linkedin: "https://www.linkedin.com/in/arpita-aparna-125b60305/",
+          github: "https://github.com/aashi411",
+        },
+      },
+      {
+        name: "Gyan Prakash",
+        image: "/ML_Gyan_Prakash.jpg",
+        social: {
+          instagram: "#",
+          linkedin: "https://www.linkedin.com/in/gyan-prakash-7bb196262",
+          github: "https://github.com/Gyanprakash136",
+        },
+      },
+      {
+        name: "Ishan Chowdhury",
+        image: "/ML_Ishan_Chowdhury.jpg",
+        social: {
+          instagram: "#",
+          linkedin: "www.linkedin.com/in/ishan-chowdhury-",
+          github: "https://github.com/ishan-chowdhury",
+        },
+      },
+      {
+        name: "Ishwar Patra",
+        image: "/ML_Ishwar_Patra.jpg",
+        social: {
+          instagram: "https://www.instagram.com/iswar_knows",
+          linkedin: "https://www.linkedin.com/in/iswar-patra-95650a336",
+          github: "https://github.com/Ishwarpatra/",
+        },
+      },
+      {
+        name: "Nikita Nayak",
+        image: "/ML_Nikita Nayak.jpg",
+        social: {
+          instagram: "#",
+          linkedin: "https://www.linkedin.com/in/nikita-nayak-4b46b8261",
+          github: "https://github.com/nayakiniki",
+        },
+      },
+      {
+        name: "Prerit Arya",
+        image: "/ML_Prerit Arya.jpg",
+        social: {
+          instagram: "https://www.instagram.com/prerit_arya_",
+          linkedin: "https://www.linkedin.com/in/prerit-arya-86a57427a",
+          github: "#",
+        },
+      },
+      {
+        name: "Suprovo Mallick",
+        image: "/ML_Suprovo_Mallick.jpg",
+        social: {
+          instagram: "#",
+          linkedin: "https://www.linkedin.com/in/suprovo-mallick-abb582287",
+          github: "https://github.com/DYNOSuprovo",
+        },
+      },
+    ],
+  },
+  "Mobile Development": {
+    icon: Smartphone,
+    color: "from-[#FF8820] to-[#8d0e0e]",
+    lead: {
+      name: "Kartavya",
+      role: "LEAD",
+      image: "/demo.png",
+      social: {
+        instagram: "#",
+        linkedin: "#",
+        github: "#",
+      },
+    },
+    members: [
+      {
+        name: "Ritam Patra",
+        image: "/APP_Ritam_Patra.jpg",
+        social: {
+          instagram: "#",
+          linkedin: "https://www.linkedin.com/in/ritam-patra/",
+          github: "https://github.com/RitamPatra",
+        },
+      },
+    ],
+  },
+  Cybersecurity: {
+    icon: Shield,
+    color: "from-[#fa5935] to-[#8d0e0e]",
+    lead: {
+      name: "Adwika Sarraf",
+      role: "LEAD",
+      image: "/CYBER_Adwika_Sarraf.jpg",
+      social: {
+        instagram: "#",
+        linkedin: "https://www.linkedin.com/in/adwika-sarraf-b8893326b/",
+        github: "#",
+      },
+    },
+    members: [
+      {
+        name: "Ananya Panda",
+        image: "/CYBER_Ananya_Panda.jpg",
+        social: {
+          instagram: "#",
+          linkedin: "https://www.linkedin.com/in/ananya-panda-5434b8357",
+          github: "https://github.com/Ananyaaa31",
+        },
+      },
+      {
+        name: "Arghya Bose",
+        image: "/CYBER_Arghya_Bose.jpg",
+        social: {
+          instagram: "#",
+          linkedin: "#",
+          github: "#",
+        },
+      },
+      {
+        name: "Arin Srivastava",
+        image: "/CYBER_Arin_Srivastava.jpg",
+        social: {
+          instagram: "https://www.instagram.com/arrinnn.",
+          linkedin: "https://www.linkedin.com/in/arin-srivastava-672947309",
+          github: "#",
+        },
+      },
+      {
+        name: "Ishan Gupta",
+        image: "/CYBER_Ishan_Gupta.jpg",
+        social: {
+          instagram: "#",
+          linkedin: "https://www.linkedin.com/in/ishan-gupta-962a07285/",
+          github: "https://github.com/CoderIshanGupta",
+        },
+      },
+      {
+        name: "Srikanth J",
+        image: "/CYBER_Srikanth_J.jpeg",
+        social: {
+          instagram: "#",
+          linkedin: "#",
+          github: "#",
+        },
+      },
+    ],
+  },
+  "Design & Branding": {
+    icon: Smartphone,
+    color: "from-[#FF8820] to-[#8d0e0e]",
+    lead: {
+      name: "Krrish Chopra",
+      role: "LEAD",
+      image: "/DESIGN_Krrish_Chopra.jpg",
+      social: {
+        instagram: "#",
+        linkedin: "https://www.linkedin.com/in/krrishchopra7",
+        github: "#",
+      },
+    },
+    members: [
+      {
+        name: "Anusuya Srivastava",
+        image: "/DESIGN_Anusuya_Srivastava.jpg",
+        social: {
+          instagram: "#",
+          linkedin: "https://www.linkedin.com/in/anusuyasrivastava",
+          github: "https://github.com/anusuyasri",
+        },
+      },
+      {
+        name: "Rupali Pasa",
+        image: "/DESIGN_Rupali_Pasa.jpg",
+        social: {
+          instagram: "https://www.instagram.com/rupalipasa/",
+          linkedin: "https://www.linkedin.com/in/rupali-pasa-118a44365/",
+          github: "#",
+        },
+      },
+      {
+        name: "Sahithi Jalaparti",
+        image: "/DESIGN_Sahithi_Jalaparti.jpg",
+        social: {
+          instagram: "https://www.instagram.com/itss_sahithii/",
+          linkedin: "https://www.linkedin.com/in/sahithi-jalaparti-a7a331214/",
+          github: "#",
+        },
+      },
+    ],
+  },
+  "Competitive Programming": {
+    icon: Smartphone,
+    color: "from-[#FF8820] to-[#8d0e0e]",
+    lead: {
+      name: "Ayush Suman",
+      role: "LEAD",
+      image: "/CP_Ayush_Suman.jpg",
+      social: {
+        instagram: "#",
+        linkedin: "https://www.linkedin.com/in/ayushsuman001",
+        github: "https://github.com/ayushsuman001",
+      },
+    },
+    members: [
+      {
+        name: "Aditya Roy",
+        image: "/CP_Aditya_Roy.jpeg",
+        social: {
+          instagram: "#",
+          linkedin: "https://www.linkedin.com/in/aditya-roy18",
+          github: "https://github.com/Adityaroy000",
+        },
+      },
+      {
+        name: "Arka Routh",
+        image: "/CP_Arka_Routh.jpg",
+        social: {
+          instagram: "https://www.instagram.com/arka_routh",
+          linkedin: "https://www.linkedin.com/in/arka-routh-6b9aba343",
+          github: "#",
+        },
+      },
+      {
+        name: "Kshitij Kumar",
+        image: "/CP_Kshitij_Kumar.jpg",
+        social: {
+          instagram: "#",
+          linkedin: "https://www.linkedin.com/in/kshitij-kumar04/",
+          github: "https://github.com/Kshitij1224",
+        },
+      },
+      {
+        name: "Mohammad Zeeshan Ahmad",
+        image: "/CP_Mohammad_Zeeshan_Ahmad.jpg",
+        social: {
+          instagram: "#",
+          linkedin: "https://www.linkedin.com/in/mohammadzeeshanahmad-/",
+          github: "https://github.com/zee10524",
+        },
+      },
+      {
+        name: "Saanket Mondal",
+        image: "/CP_Saanket_Mondal.jpg",
+        social: {
+          instagram: "#",
+          linkedin: "https://www.linkedin.com/in/mondalsaanket",
+          github: "#",
+        },
+      },
+      {
+        name: "Shubham Kumar",
+        image: "/CP_Shubham_Kumar.jpg",
+        social: {
+          instagram: "https://www.instagram.com/_.shubh.u_/",
+          linkedin: "https://www.linkedin.com/in/shubham-kumar-177060345/",
+          github: "#",
+        },
+      },
+      {
+        name: "Sourav Paul",
+        image: "/CP_Sourav_Paul.jpeg",
+        social: {
+          instagram: "https://www.instagram.com/souravvvvv_4",
+          linkedin: "www.linkedin.com/in/sourav-paul-64156b278",
+          github: "#",
+        },
+      },
+    ],
+  },
+  "Cloud Computing": {
+    icon: Smartphone,
+    color: "from-[#FF8820] to-[#8d0e0e]",
+    lead: {
+      name: "Ankush saini",
+      role: "LEAD",
+      image: "/demo.png",
+      social: {
+        instagram: "#",
+        linkedin: "#",
+        github: "#",
+      },
+    },
+    members: [
+      {
+        name: "Ritwik Rish Raj",
+        image: "/CLOUD_Ritwik_Rish_Raj.jpg",
+        social: {
+          instagram: "#",
+          linkedin: "https://www.linkedin.com/in/ritwik-rish-raj-4880a8322/",
+          github: "https://github.com/RitwikRishRaj",
+        },
+      },
+      {
+        name: "Sourish Dey",
+        image: "/CLOUD_Sourish_Dey.jpg",
+        social: {
+          instagram: "#",
+          linkedin: "https://www.linkedin.com/in/sourish-dey-20b170206",
+          github: "#",
+        },
+      },
+    ],
+  },
+  "Marketing & PR": {
+    icon: Smartphone,
+    color: "from-[#FF8820] to-[#8d0e0e]",
+    lead: {
+      name: "Ary Bhatia",
+      role: "LEAD",
+      image: "/demo.png",
+      social: {
+        instagram: "#",
+        linkedin: "#",
+        github: "#",
+      },
+    },
+    members: [
+      {
+        name: "Mayank Malik",
+        image: "/MARKETING_Mayank_Malik.jpeg",
+        social: {
+          instagram: "https://www.instagram.com/mayank.19__",
+          linkedin: "https://linkedin.com/in/mayankmalik3009",
+          github: "#",
+        },
+      },
+      {
+        name: "Ritesh Kumar Singh",
+        image: "/MARKETING_Ritesh_Kumar_Singh.jpg",
+        social: {
+          instagram: "#",
+          linkedin: "https://www.linkedin.com/in/ritesh-kumar-singh-2228b031b",
+          github: "#",
+        },
+      },
+      {
+        name: "Riya Rastogi",
+        image: "/MARKETING_Riya_Rastogi.jpg",
+        social: {
+          instagram: "#",
+          linkedin: "#",
+          github: "#",
+        },
+      },
+      {
+        name: "Sanket Kumar Gupta",
+        image: "/MARKETING_Sanket_Kumar_Gupta.jpg",
+        social: {
+          instagram: "#",
+          linkedin: "#",
+          github: "#",
+        },
+      },
+    ],
+  },
+  Administration: {
+    icon: Smartphone,
+    color: "from-[#FF8820] to-[#8d0e0e]",
+    lead: {
+      name: "Shubham Agarwal",
+      role: "LEAD",
+      image: "/demo.png",
+      social: {
+        instagram: "#",
+        linkedin: "#",
+        github: "#",
+      },
+    },
+    members: [
+      {
+        name: "Navprabhat Mishra",
+        image: "/ADMIN_Navprabhat_Mishra.jpeg",
+        social: {
+          instagram: "#",
+          linkedin: "#",
+          github: "#",
+        },
+      },
+      {
+        name: "Ronit Chandra",
+        image: "/ADMIN_Ronit_Chandra.jpg",
+        social: {
+          instagram: "https://www.instagram.com/ronith.chandra",
+          linkedin: "https://www.linkedin.com/in/ronith-chandra-2016a2283",
+          github: "#",
+        },
+      },
+    ],
+  },
+  "Content Writing": {
+    icon: Smartphone,
+    color: "from-[#FF8820] to-[#8d0e0e]",
+    lead: {
+      name: "Arghajit",
+      role: "LEAD",
+      image: "/demo.png",
+      social: {
+        instagram: "#",
+        linkedin: "#",
+        github: "#",
+      },
+    },
+    members: [
+      {
+        name: "Pushpita Ghosh",
+        image: "/CONTENT_Pushpita_Ghosh.jpg",
+        social: {
+          instagram: "https://www.instagram.com/virtue_in_velvet",
+          linkedin: "https://www.linkedin.com/in/pushpita-ghosh-0394762aa",
+          github: "#",
+        },
+      },
+      {
+        name: "Swayam Gupta",
+        image: "/CONTENT_Swayam_Gupta.jpeg",
+        social: {
+          instagram: "#",
+          linkedin: "https://www.linkedin.com/in/swayam-gupta-a62b66329/",
+          github: "https://github.com/GSWAYAM9",
+        },
+      },
+    ],
+  },
+};
+
+// Backward compatibility exports
+export const leadershipData = leadership;
+export const domainsData = domains;
+
+const memberData2025 = { leadership, domains };
+export default memberData2025;
