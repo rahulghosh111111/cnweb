5f88076fa5f06080a2c9a53f317423e4afc60a35
Soumyadip Maity <22053029@kiit.ac.in>
2025-07-29 01:00:44 +0530

    Implement popup functionality in Home component

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,5 @@
+"use client"
+import React, { useState, useEffect } from "react";
 import HeroSection from "@/components/Landing/Herosection";
 import OurNinjas from "@/components/Landing/OurNinjas";
 import OurEvents from "@/components/Landing/OurEvents";
@@ -6,12 +8,18 @@ import Alumini from "@/components/Landing/Alumini";
 import JoinTeam from "@/components/Landing/JoinTeam";
 import AboutSection from "@/components/Landing/AboutSection";
 import AnimatedTestimonialsDemo from "@/components/Landing/new-testimonial";
+import Popup from "@/components/Landing/Popup";
 
 export default function Home() {
+  const [showPopup, setShowPopup] = useState(false);
+
+  useEffect(() => {
+    setShowPopup(true);
+  }, []);
+
   return (
-
     <div>
-
+      {showPopup && <Popup onClose={() => setShowPopup(false)} />}
       <HeroSection />
       <AboutSection />
       <OurNinjas />
@@ -22,4 +30,4 @@ export default function Home() {
       <JoinTeam/>
     </div>
   );
-} 
+}
